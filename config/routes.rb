ee58d5d2d0dce264cd750972367d061d072fc06a
SYMATRIXgit config --global user.name SYMATRIXgit config --global user.email symatrixing@gmail.com <symatrixing@gmail.com>
2017-06-15 12:21:58 +1200

    3.10 modified route file second time

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/menu'
-  root 'static_pages#menu'
-
-  get 'static_pages/about'
-  root 'static_pages#about'
-
-  get 'static_pages/contact'
-  root 'static_pages#contact'
-
-  get 'static_pages/index'
   root 'static_pages#index'
-
-
-
+  get '/static_pages/index'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/menu'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
