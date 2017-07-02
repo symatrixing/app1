04302261f6f8fd899391a00d01d3e681e0bf35e7
SYMATRIXgit config --global user.name SYMATRIXgit config --global user.email symatrixing@gmail.com <symatrixing@gmail.com>
2017-07-02 14:35:57 +1200

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   get 'static_pages/menu'
   get 'products/index'
+  get 'products/show'
+  get 'products/edit'
+  get 'products/new'
+  get 'product/_form'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
