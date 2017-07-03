45153d1a286869982097a901f50a1bc15c59317b
SYMATRIXgit config --global user.name SYMATRIXgit config --global user.email symatrixing@gmail.com <symatrixing@gmail.com>
2017-07-03 20:38:03 +1200

    4.3 created orders pg

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :products
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
   get '/static_pages/index'
   get 'static_pages/about'
   get 'static_pages/contact'
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get 'products/new'
   get 'product/_form'
   
+  resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
