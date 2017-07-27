ce5182f489c0886171255e6fc563f7e029b0e3e4
SYMATRIXgit config --global user.name SYMATRIXgit config --global user.email symatrixing@gmail.com <symatrixing@gmail.com>
2017-07-28 00:30:55 +1200

    4.5 views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
+  layout "static"
+  
   def index
   end
 
   def landing_page 
-  	@featured_product = Product.first
+  	@products = Product.limit(3)
   end 
   
 end
