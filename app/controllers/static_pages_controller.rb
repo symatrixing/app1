a054e2fec560c41c2e79577e214ffe7acb385b56
SYMATRIXgit config --global user.name SYMATRIXgit config --global user.email symatrixing@gmail.com <symatrixing@gmail.com>
2017-09-06 20:49:36 +1200

    5.3 mailers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,5 +14,14 @@ class StaticPagesController < ApplicationController
   def contact
   end
   
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: 'symatrixing@gmail.com', 
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
   get 'static_pages/menu'
+  post 'static_pages/thank_you'
   
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
